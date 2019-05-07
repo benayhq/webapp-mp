e7654663b1b56e4bc3931242ee32f5937fe92383
向雄 <xx090304@163.com>
2019-05-07 10:35:17 +0800

    update product detail

---
+++ src/pages/product/detail.js
@@ -0,0 +1,16 @@
+import Taro,{Component} from '@tarojs/taro';
+import {View,Text} from '@tarojs/components';
+import './detail.scss';
+
+class Detail extends Component{
+
+    render(){
+        return (
+            <View>
+                Detail
+            </View>
+        )
+    }
+}
+
+export default Detail;
